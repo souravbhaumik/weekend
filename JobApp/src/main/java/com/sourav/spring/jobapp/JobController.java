19fbed8ed1fb11347c18a336cdc3b69657bf988d
Sourav Bhaumik <souravbhaumik0812@gmail.com>
2025-09-06 07:00:18 +0530

    FIXED TO WORKING

--- JobApp/src/main/java/com/sourav/spring/jobapp/JobController.java
+++ JobApp/src/main/java/com/sourav/spring/jobapp/JobController.java
@@ -1,6 +1,8 @@
 package com.sourav.spring.jobapp;
 
+import com.sourav.spring.jobapp.model.JobPost;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +11,16 @@ public class JobController {
     @RequestMapping({"/", "home"})
     public String home() {
         return "home";
+    }
+
+    @RequestMapping("addjob")
+    public String addJob() {
+        return "addjob";
+    }
+
+    @PostMapping("handleForm")
+    public String handleForm(JobPost jobPost) {
+        return "success";
     }
 
 }
---
+++ JobApp/src/main/java/com/sourav/spring/jobapp/model/JobPost.java
@@ -0,0 +1,22 @@
+package com.sourav.spring.jobapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+public class JobPost {
+
+    private int postId;
+    private String postProfile;
+    private String postDesc;
+    private Integer reqExperience;
+    private List<String> postTechStack;
+
+}
